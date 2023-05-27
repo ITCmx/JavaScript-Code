7b2268d605ca7842758e17f53c35cdc112122c7b
Oscar <oesquedac@gmail.com>
2023-05-27 10:34:36 -0600

    Update ITCmxDates.js

--- ITCmxDates.js
+++ ITCmxDates.js
@@ -1,3 +1,16 @@
+function obtenerFechaActual() {
+  var fechaActual = new Date();
+  
+  var dia = fechaActual.getDate();
+  var mes = fechaActual.getMonth() + 1; // Los meses comienzan en 0, por lo tanto se suma 1
+  var anio = fechaActual.getFullYear();
+  
+  // Formatear la fecha como una cadena
+  var fechaActualFormateada = dia + '/' + mes + '/' + anio;
+  
+  return fechaActualFormateada;
+}
+
 function calcularDiasEntreFechas(fecha1, fecha2) {
   // Convertir las fechas a objetos Date
   var fechaInicio = new Date(fecha1);
