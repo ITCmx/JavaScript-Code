c1a9db1af191d12687f46683f30c429cb049315c
Oscar <oesquedac@gmail.com>
2023-05-27 10:24:06 -0600

    Create ITCmxDates.js

---
+++ ITCmxDates.js
@@ -0,0 +1,13 @@
+function calcularDiasEntreFechas(fecha1, fecha2) {
+  // Convertir las fechas a objetos Date
+  var fechaInicio = new Date(fecha1);
+  var fechaFin = new Date(fecha2);
+
+  // Calcular la diferencia en milisegundos
+  var diferencia = fechaFin.getTime() - fechaInicio.getTime();
+
+  // Convertir la diferencia a días
+  var dias = Math.ceil(diferencia / (1000 * 3600 * 24));
+
+  return dias;
+}
